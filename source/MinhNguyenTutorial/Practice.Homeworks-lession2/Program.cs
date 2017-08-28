eadc967989667c3684ee96c587fe16a404525595
minh21296@gmail.com <khongbiet1>
2017-08-28 10:24:22 -0700

    complete Homework

---
+++ source/MinhNguyenTutorial/Practice.Homeworks-lession2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Homeworks_lession2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task();
+            Console.ReadLine();
+        }
+
+        private static void Task()
+        {
+            Console.WriteLine("Employee List :");
+            var employees = GetEmployee();
+            PrintEmployee(employees);
+            
+
+        }
+        private static List<Employee> GetEmployee() {
+           
+            var employee1 = new Employee { EmployeeID = 1 , LastName= "Davolio" , FristName= "Nancy",Title= "Sales Representative",TitleOfCourtesy="Ms.",BirthDate= "1948-12-08 0:00:00" ,Address= "507 - 20th Ave. E.Apt. 2A" ,City= "Seattle",Region= "WA" ,PostalCode= "98122",Country= "USA",HomePhone= "(206) 555-9857", PhotoPath= "http://accweb/emmployees/davolio.bmp" };
+            var employee2 = new Employee { EmployeeID = 2, LastName = "Davolio", FristName = "Nancy", Title = "Sales Representative", TitleOfCourtesy = "Ms.", BirthDate = "1948-12-08 0:00:00", Address = "507 - 20th Ave. E.Apt. 2A", City = "Seattle", Region = "WA", PostalCode = "98122", Country = "USA", HomePhone = "(206) 555-9857", PhotoPath = "http://accweb/emmployees/davolio.bmp" };
+            var employees = new List<Employee> { employee1, employee2 };
+
+            return employees;
+        }
+
+
+        private     static void PrintEmployee(List<Employee> employees)
+        {   
+            foreach( var employee in employees)
+            {
+                Console.WriteLine(employee.GetDetail());
+            }
+        }
+      
+    }
+}
