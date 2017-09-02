52aab240960001759ac90be839a858f27763ebc5
minh21296@gmail.com <khongbiet1>
2017-09-02 10:30:18 +0700

    learn4

--- source/MinhNguyenTutorial/EmployeeManagementApp/Form1.Designer.cs
+++ source/MinhNguyenTutorial/EmployeeManagementApp/Form1.Designer.cs
@@ -30,12 +30,14 @@
         {
             this.lblTitle = new System.Windows.Forms.Label();
             this.grdEmployee = new System.Windows.Forms.DataGridView();
-            this.btnSearch = new System.Windows.Forms.Button();
-            this.txtsearch = new System.Windows.Forms.TextBox();
             this.FullName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.BirthDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.FullAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtsearch = new System.Windows.Forms.TextBox();
+            this.btnExportToTxt = new System.Windows.Forms.Button();
+            this.btnExportToCsv = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.grdEmployee)).BeginInit();
             this.SuspendLayout();
             // 
@@ -66,24 +68,6 @@
             this.grdEmployee.Size = new System.Drawing.Size(794, 429);
             this.grdEmployee.TabIndex = 1;
             // 
-            // btnSearch
-            // 
-            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSearch.Location = new System.Drawing.Point(686, 85);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(139, 37);
-            this.btnSearch.TabIndex = 2;
-            this.btnSearch.Text = "Search";
-            this.btnSearch.UseVisualStyleBackColor = true;
-            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
-            // 
-            // txtsearch
-            // 
-            this.txtsearch.Location = new System.Drawing.Point(376, 85);
-            this.txtsearch.Name = "txtsearch";
-            this.txtsearch.Size = new System.Drawing.Size(290, 20);
-            this.txtsearch.TabIndex = 3;
-            // 
             // FullName
             // 
             this.FullName.DataPropertyName = "FullName";
@@ -111,17 +95,59 @@
             this.FullAddress.Name = "FullAddress";
             this.FullAddress.Width = 350;
             // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch.Location = new System.Drawing.Point(686, 85);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(139, 37);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtsearch.Location = new System.Drawing.Point(376, 85);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(290, 20);
+            this.txtsearch.TabIndex = 3;
+            // 
+            // btnExportToTxt
+            // 
+            this.btnExportToTxt.Location = new System.Drawing.Point(42, 59);
+            this.btnExportToTxt.Name = "btnExportToTxt";
+            this.btnExportToTxt.Size = new System.Drawing.Size(125, 38);
+            this.btnExportToTxt.TabIndex = 4;
+            this.btnExportToTxt.Text = "Export to Text";
+            this.btnExportToTxt.UseVisualStyleBackColor = true;
+            this.btnExportToTxt.Click += new System.EventHandler(this.btnExportToTxt_Click);
+            // 
+            // btnExportToCsv
+            // 
+            this.btnExportToCsv.Location = new System.Drawing.Point(173, 59);
+            this.btnExportToCsv.Name = "btnExportToCsv";
+            this.btnExportToCsv.Size = new System.Drawing.Size(125, 38);
+            this.btnExportToCsv.TabIndex = 4;
+            this.btnExportToCsv.Text = "Export to Csv";
+            this.btnExportToCsv.UseVisualStyleBackColor = true;
+            this.btnExportToCsv.Click += new System.EventHandler(this.btnExportToCsv_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(837, 569);
+            this.Controls.Add(this.btnExportToCsv);
+            this.Controls.Add(this.btnExportToTxt);
             this.Controls.Add(this.txtsearch);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.grdEmployee);
             this.Controls.Add(this.lblTitle);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.OnFormLoaded);
             ((System.ComponentModel.ISupportInitialize)(this.grdEmployee)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -138,6 +164,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Title;
         private System.Windows.Forms.DataGridViewTextBoxColumn BirthDate;
         private System.Windows.Forms.DataGridViewTextBoxColumn FullAddress;
+        private System.Windows.Forms.Button btnExportToTxt;
+        private System.Windows.Forms.Button btnExportToCsv;
     }
 }
 
--- source/MinhNguyenTutorial/EmployeeManagementApp/Form1.cs
+++ source/MinhNguyenTutorial/EmployeeManagementApp/Form1.cs
@@ -1,69 +1,148 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows.Forms;
+using EmployeeManagementApp.Repository;
+using System.Linq;
+using System.Collections.Generic;
 using EmployeeManagementApp.Model;
+using System.Text;
+using System.IO;
 
 namespace EmployeeManagementApp
 {
     public partial class Form1 : Form
     {
+        private readonly EmployeeRepository _employeeRepository;//Field
+                        
         public Form1()
         {
             InitializeComponent();
             grdEmployee.AutoGenerateColumns = false;
-            var employees = GetEmployees();
-            grdEmployee.DataSource = employees;
-        }
-
-        private List<Employee> GetEmployees()
-        {
-            var date = new DateTime(1948, 12, 08);
-            var date1 = new DateTime(1952, 02, 19);
-            var date2 = new DateTime(1963, 08, 30);
-            var date3 = new DateTime(1937, 09, 19);
-            var date4 = new DateTime(1955, 03, 04);
-            var date5 = new DateTime(1963, 07, 02);
-            var date6 = new DateTime(1960, 05, 29);
-            var date7 = new DateTime(1958, 01, 09);
-            var date8 = new DateTime(1966, 01, 27);
-
-            var employee1 = new Employee { EmployeeID = 1, LastName = "Davolio", FristName = "Nancy", Title = "Sales Representative", TitleOfCourtesy = "Ms.", BirthDate = date, Address = "507 - 20th Ave. E.Apt. 2A", City = "Seattle", Region = "WA", PostalCode = "98122", Country = "USA", HomePhone = "(206) 555-9857", PhotoPath = "http://accweb/emmployees/davolio.bmp" };
-            var employee2 = new Employee { EmployeeID = 2, LastName = "Fuller", FristName = "Andrew", Title = "Vice President, Sales", TitleOfCourtesy = "Dr.", BirthDate = date1, Address = "908 W. Capital Way", City = "Tacoma", Region = "WA", PostalCode = "98401", Country = "USA", HomePhone = "(206) 555-9482", PhotoPath = "http://accweb/emmployees/davolio.bmp" };
-            var employee3 = new Employee { EmployeeID = 3, LastName = "Leverling", FristName = "Janet", Title = "Sales Representative", TitleOfCourtesy = "Ms.", BirthDate = date2, Address = "722 Moss Bay Blvd.", City = "Kirkland", Region = "WA", PostalCode = "98033", Country = "USA", HomePhone = "(206) 555 - 3412", PhotoPath = "http://accweb/emmployees/fuller.bmp" };
-            var employee4 = new Employee { EmployeeID = 4, LastName = "Peacock", FristName = "Margaret", Title = "Sales Representative", TitleOfCourtesy = "Mrs.", BirthDate = date3, Address = "4110 Old Redmond Rd.", City = "Redmond", Region = "WA", PostalCode = "98052", Country = "USA", HomePhone = "(206) 555-8122", PhotoPath = "http://accweb/emmployees/leverling.bmp" };
-            var employee5 = new Employee { EmployeeID = 5, LastName = "Buchanan", FristName = "Steven", Title = "Sales Manager", TitleOfCourtesy = "Mr.", BirthDate = date4, Address = "14 Garrett Hill", City = "London", Region = "", PostalCode = "SW1 8JR", Country = "UK", HomePhone = "(71) 555-4848", PhotoPath = "http://accweb/emmployees/peacock.bmp" };
-            var employee6 = new Employee { EmployeeID = 6, LastName = "Suyama", FristName = "Michael", Title = "Sales Representative", TitleOfCourtesy = "Mr.", BirthDate = date5, Address = "Coventry HouseMiner Rd.", City = "London", Region = "", PostalCode = "EC2 7JR", Country = "UK", HomePhone = "(71) 555-7773", PhotoPath = "http://accweb/emmployees/buchanan.bmp" };
-            var employee7 = new Employee { EmployeeID = 7, LastName = "King", FristName = "Robert", Title = "Sales Representative", TitleOfCourtesy = "Mr.", BirthDate = date6, Address = "Edgeham HollowWinchester Way", City = "London", Region = "", PostalCode = "RG1 9SP", Country = "UK", HomePhone = "(71) 555-5598", PhotoPath = "http://accweb/emmployees/davolio.bmp" };
-            var employee8 = new Employee { EmployeeID = 8, LastName = "Callahan", FristName = "Laura", Title = "Inside Sales Coordinator", TitleOfCourtesy = "Ms.", BirthDate = date7, Address = "4726 - 11th Ave. N.E.", City = "Seattle", Region = "WA", PostalCode = "98105", Country = "USA", HomePhone = "(206) 555-1189", PhotoPath = "http://accweb/emmployees/davolio.bmp" };
-            var employee9 = new Employee { EmployeeID = 9, LastName = "Dodsworth", FristName = "Anne", Title = "Sales Representative", TitleOfCourtesy = "Ms.", BirthDate = date8, Address = "7 Houndstooth Rd.", City = "London", Region = " ", PostalCode = "WG2 7LT", Country = "UK", HomePhone = "(71) 555-4444", PhotoPath = "http://accweb/emmployees/davolio.bmp" };
-            
-            var employees = new List<Employee> { employee1, employee2, employee3, employee4, employee5, employee6, employee7, employee8, employee9 };
-
-            return employees;
-
+            grdEmployee.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            _employeeRepository = new EmployeeRepository();
         }
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
+            SearchUsingLinqMethodCallStyle();
+            //SearchByUsingLinqQueryExpressionStyle();
+            //SearchByPrevLinq();
+
+            var props = typeof(Employee).GetProperties();
+        }
+
+        private void SearchByPrevLinq()
+        {
             string searchValue = txtsearch.Text;
-         grdEmployee.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            try
+            var allEmployees = _employeeRepository.GetEmployees();
+            if (!string.IsNullOrEmpty(searchValue))
             {
-                foreach (DataGridViewRow row in grdEmployee.Rows)
+                searchValue = searchValue.ToLower();
+                var filterdEmps = new List<Employee>();
+
+                foreach (var emp in allEmployees)
                 {
-                    if (row.Cells[4].Value.ToString().Equals(searchValue))
+                    if(emp.FullName.ToLower().Contains(searchValue) || emp.FullAddress.ToLower().Contains(searchValue))
                     {
-                        row.Selected = true;
-                        break;
+                        filterdEmps.Add(emp);
                     }
                 }
+
+                grdEmployee.DataSource = filterdEmps;
             }
-            catch 
+            else
             {
-                MessageBox.Show("input error");
+                grdEmployee.DataSource = allEmployees;
             }
 
+            var filteredEmployees = new List<Employee>();
 
+          
+        }
+
+        private void SearchByUsingLinqQueryExpressionStyle()
+        {
+            string searchValue = txtsearch.Text;
+            var allEmployees = _employeeRepository.GetEmployees();
+            
+            if (!string.IsNullOrEmpty(searchValue))
+            {
+                searchValue = searchValue.ToLower();
+                var filteredEmployees = from e in allEmployees
+                                        where e.FullName.ToLower().Contains(searchValue) || e.FullAddress.ToLower().Contains(searchValue)
+                                        orderby e.BirthDate descending
+                                        select e;            
+
+                grdEmployee.DataSource = filteredEmployees.ToList();
+            }
+            else
+            {
+                grdEmployee.DataSource = (from e in allEmployees orderby e.BirthDate descending select e).ToList();
+            }
+        }
+
+        private void SearchUsingLinqMethodCallStyle()
+        {
+            string searchValue = txtsearch.Text;
+            var allEmployees = _employeeRepository.GetEmployees();
+            if (!string.IsNullOrEmpty(searchValue))
+            {
+                searchValue = searchValue.ToLower();
+                var filteredEmployees = allEmployees.Where(x => x.FullName.ToLower().Contains(searchValue) || x.FullAddress.ToLower().Contains(searchValue))
+                                                    .OrderByDescending(x => x.BirthDate);
+
+                grdEmployee.DataSource = filteredEmployees.ToList();
+            }
+            else
+            {
+                grdEmployee.DataSource = allEmployees.OrderByDescending(x => x.BirthDate).ToList();
+            }
+        }
+
+        private void OnFormLoaded(object sender, EventArgs e)
+        {
+           //var employees= _employeeRepository.GetEmployees();
+           // grdEmployee.DataSource = employees;
+        }
+
+        private void btnExportToTxt_Click(object sender, EventArgs e)
+        {
+            ExportToText1();
+        }
+
+        private void ExportToText1()
+        {
+            var allEmployees = _employeeRepository.GetEmployees();
+
+            StringBuilder str = new StringBuilder();
+
+            foreach (var emp in allEmployees)
+            {
+                str.AppendLine($"Full Name: {emp.FullName} - Title: {emp.Title} - Full Address: {emp.FullAddress}");
+            }                        
+            
+            var path = $"{Directory.GetCurrentDirectory()}\\emp.txt";
+
+            File.WriteAllText(path, str.ToString());
+
+            MessageBox.Show("Successfully exported");
+        }
+
+        private void btnExportToCsv_Click(object sender, EventArgs e)
+        {
+            var allEmployees = _employeeRepository.GetEmployees();
+
+            StringBuilder str = new StringBuilder();
+
+            foreach (var emp in allEmployees)
+            {
+                str.AppendLine($"{emp.FullName};{emp.Title};{emp.FullAddress}");
+            }
+
+            var path = $"{Directory.GetCurrentDirectory()}\\emp.csv";
+
+            File.WriteAllText(path, str.ToString());
+
+            MessageBox.Show("Successfully exported");
         }
     }
 }
