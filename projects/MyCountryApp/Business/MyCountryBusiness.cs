a6855f2de22de64a3f765e60394e3edd2a57ff22
minh21296@gmail.com <khongbiet1>
2017-09-28 21:57:06 +0700

    lesson12

--- projects/MyCountryApp/Business/MyCountryBusiness.cs
+++ projects/MyCountryApp/Business/MyCountryBusiness.cs
@@ -11,27 +11,35 @@ namespace MyCountryApplication.Business
 {
     public class MyCountryBusiness
     {
-        public List<DistrictViewModel> SearchDistricts(string keyword ="",string cityCode="")
+        public List<DistrictViewModel> SearchDistricts(string keyword = "", string cityCode = "")
         {
-            var dbContext= new MyCountryEntities();
-            var query = (from d in dbContext.Districts
-                join c in dbContext.Cities on d.CityCode equals c.CityCode
-                select new DistrictViewModel()
-                {
-                    DistrictCode = d.DistrictCode,
-                    DistrictName = d.Name,
-                    CityCode = c.CityCode,
-                   CityName= c.Name
-                });
+            var dbContext = new MyCountryEntities();
+
+            //using Join
+            ////var query = (from d in dbContext.Districts
+            ////    join c in dbContext.Cities on d.CityCode equals c.CityCode
+            ////    select new DistrictViewModel()
+            ////    {
+            ////        DistrictCode = d.DistrictCode,
+            ////        DistrictName = d.Name,
+            ////        CityCode = c.CityCode,
+            ////       CityName= c.Name
+            ////    });
+
+            //not use join
+            var query = dbContext.Districts.Select(d => new DistrictViewModel { DistrictCode = d.DistrictCode, DistrictName = d.Name, CityCode = d.CityCode, CityName = d.City.Name });
+
             if (!string.IsNullOrEmpty(keyword))
             {
                 query = query.Where(d => d.DistrictCode.Contains(keyword) || d.DistrictName.Contains(keyword));
             }
             if (!string.IsNullOrEmpty(cityCode))
             {
-                query = query.Where(c=>c.CityCode==cityCode);
+                query = query.Where(c => c.CityCode == cityCode);
             }
-            var result = query.OrderBy(x=>x.DistrictCode).ToList();
+
+            var result = query.OrderBy(x => x.DistrictCode).ToList();
+
             return result;
 
         }
@@ -43,5 +51,5 @@ namespace MyCountryApplication.Business
             return cities;
         }
     }
-   
+
 }
