eb41fb06e08f15c400af1cfe4784b9afca3a3c1a
vanph <vanphbk@gmail.com>
2017-10-23 23:57:43 +0700

    Lesson 20

---
+++ source/review/Review/Sample/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    public class Employee
+    {
+        private string _firstName; //field , backing field
+
+        public string FirstName //Property
+        {
+            get
+            {
+                return _firstName;
+            }
+
+            set { _firstName = value; }
+        }
+
+        public string MiddleName { get; set; } //Auto-Property
+
+        public string LastName { get; set; }
+
+        public string FullName => string.IsNullOrEmpty(MiddleName) ? $"{FirstName} {LastName}" : $"{FirstName} {MiddleName} {LastName}";
+
+        public Employee()
+        {
+
+        }
+
+        //Constructor
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Employee(string firstName, string middleName, string lastName)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+        }
+
+        public void SayHello()//method
+        {
+            Console.WriteLine($"Hello {FullName}");
+        }
+    }
+}
---
+++ source/review/Review/Sample/StringExtenstion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    public static class TestExtenstion
+    {
+        public static int CoundWord(this string str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' },StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static string Hello(this string str)
+        {
+            return $"Hello {str}";
+        }
+
+        public static int MultiplyBy(this int number, int factor)
+        {
+            return number * factor;
+        }
+    }
+}
